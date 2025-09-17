b12a7f366e11655cdd95fbd2aafb2e8ebad1d9f9
saintlee83 <heisasaint83@gmail.com>
2025-09-17 23:35:22 +0900

    Build my first crewai agent

--- main.py
+++ main.py
@@ -1,4 +1,56 @@
+import os
 from crewai import Crew, Agent, Task
+from crewai.project import CrewBase, agent, task, crew
+from tools import count_letters
 from dotenv import load_dotenv
 
 load_dotenv()
+
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+
+@CrewBase
+class TranslatorCrew:
+
+    @agent
+    def translator_agent(self):
+        return Agent(
+            config=self.agents_config["translator_agent"],
+        )
+
+    @agent
+    def counter_agent(self):
+        return Agent(
+            config=self.agents_config["counter_agent"],
+            tools=[count_letters]
+        )
+
+    @task
+    def translate_task(self):
+        return Task(
+            config=self.tasks_config["translate_task"],
+        )
+
+    @task
+    def retranslate_task(self):
+        return Task(
+            config=self.tasks_config["retranslate_task"],
+        )
+
+    @task
+    def count_tesk(self):
+        return Task(
+            config=self.tasks_config["count_task"],
+        )
+
+    @crew
+    def assemble_crew(self):
+        return Crew(
+            agents=self.agents,
+            tasks=self.tasks,
+            verbose=True,
+        )
+
+
+TranslatorCrew().assemble_crew().kickoff(
+    inputs={"sentence": "I am Jeseok Lee and I like to play basketball."})
